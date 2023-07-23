cc333f350971f90f442defa06f64cc46b81e444e
Hridoy Ahmed <hridoyahmed47@gmail.com>
2023-07-23 20:38:15 +0600

    almost done except portfolio and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,33 @@ btn.addEventListener('click', function () {
     } else {
         menu.src = "assets/image/menu-close.svg";
     }
-})
+});
+
+// for form placeholder hide event 
+let labelAll = document.querySelectorAll('.form__label');
+let inputAll = document.querySelectorAll('.form__input');
+
+console.log(labelAll, inputAll);
+
+function placeholderActive(el) {
+    // console.log(el);
+    const label = document.querySelector(`label[for="${el.target.id}"]`);
+    if (el.target.value === '') {
+        // console.log(el.getAttribute('placeholder'));
+        // console.log(el.value === '');
+        label.style.opacity = "0";
+        label.style.visibility = "hidden";
+        label.style.transform = "translateY(20PX)";
+        
+    } else {
+        label.style.opacity = "1";
+        label.style.visibility = "visible";
+        label.style.transform = "translateY(0px)";   
+    }
+};
+
+inputAll.forEach(input=> {
+    input.addEventListener('input', placeholderActive);
+    placeholderActive({ target: input})
+});
+
