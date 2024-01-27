ea54d663f9f28878b924df080c71056b9a4308b1
Hridoy Ahmed <hridoyahmed47@gmail.com>
2024-01-27 23:52:23 +0600

    custom cursor with progressbar, text slider, form validation with google sheet connecting done

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,40 +4,55 @@
 let menu = document.querySelector(".navbar-toggler-icon--img");
 let btn = document.querySelector(".navbar-toggler");
 
-btn.addEventListener('click', function () {
-    console.log(menu.src);
-    if (!btn.classList.contains("collapsed")) {
-        menu.src = "assets/image/menu-open.svg";
-    } else {
-        menu.src = "assets/image/menu-close.svg";
-    }
+btn.addEventListener("click", function () {
+  console.log(menu.src);
+  if (!btn.classList.contains("collapsed")) {
+    menu.src = "assets/image/menu-open.svg";
+  } else {
+    menu.src = "assets/image/menu-close.svg";
+  }
 });
 
-// for form placeholder hide event 
-let labelAll = document.querySelectorAll('.form__label');
-let inputAll = document.querySelectorAll('.form__input');
+// for form placeholder hide event
+let labelAll = document.querySelectorAll(".form__label");
+let inputAll = document.querySelectorAll(".form__input");
 
 console.log(labelAll, inputAll);
 
 function placeholderActive(el) {
-    // console.log(el);
-    const label = document.querySelector(`label[for="${el.target.id}"]`);
-    if (el.target.value === '') {
-        // console.log(el.getAttribute('placeholder'));
-        // console.log(el.value === '');
-        label.style.opacity = "0";
-        label.style.visibility = "hidden";
-        label.style.transform = "translateY(20PX)";
-        
-    } else {
-        label.style.opacity = "1";
-        label.style.visibility = "visible";
-        label.style.transform = "translateY(0px)";   
-    }
-};
+  // console.log(el);
+  const label = document.querySelector(`label[for="${el.target.id}"]`);
+  if (el.target.value === "") {
+    // console.log(el.getAttribute('placeholder'));
+    // console.log(el.value === '');
+    label.style.opacity = "0";
+    label.style.visibility = "hidden";
+    label.style.transform = "translateY(20PX)";
+  } else {
+    label.style.opacity = "1";
+    label.style.visibility = "visible";
+    label.style.transform = "translateY(0px)";
+  }
+}
 
-inputAll.forEach(input=> {
-    input.addEventListener('input', placeholderActive);
-    placeholderActive({ target: input})
+inputAll.forEach((input) => {
+  input.addEventListener("input", placeholderActive);
+  placeholderActive({ target: input });
 });
 
+// form submission prevent default
+
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      form.reset();
+    });
+  });
+});
