6fedf4ad74f61dc1c89e89135a8acb7ff54bca9d
Hridoy Ahmed <hridoyahmed47@gmail.com>
2024-02-10 22:19:42 +0600

    lazy loading, section revealer added. Image is optimised now

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,5 +87,60 @@ window.addEventListener("load", function () {
 /////////////////////////////////////////////////////////
 /////////////////////////////
 
-// form date sender 
-document.getElementById('date').value = Date().toLocaleDateString();
+// form date sender
+document.getElementById("date").value = Date();
+
+//////////////////////////////////////////////////
+////////////////////////
+
+// section revealer
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+////////////////////////////////////////////////////////////////
+/////////////////////////////
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
